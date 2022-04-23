46bccae54474e42cf09e042522d6c2a76cd9c8d4
komar.izmi <komar.izmi@gmail.com>
2022-04-23 15:38:29 +0700

    context

---
+++ pages/context/index.tsx
@@ -0,0 +1,12 @@
+import Container from "../../components/layout/container";
+import Wrapper from "../../components/layout/wrapper";
+
+export default function PageContext() {
+  return (
+    <Wrapper>
+      <Container>
+        <h1 className="text-2xl text-slate-700 font-light">Page context</h1>
+      </Container>
+    </Wrapper>
+  );
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -17,6 +17,12 @@ export default function Homepage() {
                 Counter
               </a>
             </Link>
+
+            <Link href={"/context"}>
+              <a className="px-3 py-1 bg-blue-500 text-white hover:bg-blue-600 rounded">
+                Context
+              </a>
+            </Link>
           </div>
         </Container>
       </Wrapper>
